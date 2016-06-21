2cab662009e5fd639b4afb6b9fa8491becde0acc
Rosiene <rosiene.dilly@gmail.com>
2016-06-21 16:38:42 +0200

    post page

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   mount_uploader :image, ImageUploader
   belongs_to :user
-  has_many :favorites
+  has_many :comments
   has_many :likes
+  has_many :favorites
 end
