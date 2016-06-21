a4be9da1801c34192b7a29d547b2af58653b63f7
Rosiene <rosiene.dilly@gmail.com>
2016-06-21 14:23:51 +0200

    All scaffold

---
+++ db/migrate/20160621121425_add_column_to_users.rb
@@ -0,0 +1,9 @@
+class AddColumnToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :avatar, :string
+    add_column :users, :name, :string
+    add_column :users, :description, :text
+    add_column :users, :gender, :string
+    add_column :users, :location, :string
+  end
+end
