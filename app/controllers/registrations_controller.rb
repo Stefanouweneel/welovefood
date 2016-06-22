27df64ed19a0b5de956fabed8aa909173a6fc5a8
Stefan Ouweneel <stefanouweneel@gmail.com>
2016-06-22 12:02:53 +0200

    Make edit page save changes

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,12 +5,18 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def update
+  @user = User.find(current_user.id)
+    if @user.update(sign_up_params)
+      redirect_to profile_path
+    else
+      render "edit"
+    end
   end
 
   private
 
   def sign_up_params
-  params.require(:user).permit(:name, :description, :email, :password, :password_confirmation, :avatar, :gender, :location)
+  params.require(:user).permit(:name, :description, :email, :avatar, :gender, :location)
   end
 
 end
