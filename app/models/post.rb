423c0b8f53cfd2e391adaad22125fe9589ea3409
Stefan Ouweneel <stefanouweneel@gmail.com>
2016-06-24 09:25:53 +0200

    Add search function to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,9 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :favorites, dependent: :destroy
+
+  def self.search(search)
+  where("title LIKE ? OR description LIKE ?",
+    "%#{search}%", "%#{search}%")
+  end
 end
